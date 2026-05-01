0d89bdef7d6759753a8e0176afbaa69a4a73d6ff
sammkaiserr <shyamsujith20@gmail.com>
2026-05-01 21:26:40 +0530

    Initial Commit

---
+++ CafeManager/CafeManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CafeManagerApp.swift
+//  CafeManager
+//
+//  Created by Shyamsujith Pochcigani on 01/05/26.
+//
+
+import SwiftUI
+
+@main
+struct CafeManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CafeManager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CafeManager
+//
+//  Created by Shyamsujith Pochcigani on 01/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
